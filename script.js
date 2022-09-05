9050cc0808b3f6bdbfd5672f13cd5044eba1ff55
jawnpol <jpcail.jpc@gmail.com>
2022-09-04 19:40:25 -0700

    Added display of numbers and hover effect on calculator

--- script.js
+++ script.js
@@ -34,3 +34,39 @@ function sub(n1, n2) {
             alert("Something has gone wrong");
     }
  }
+
+ function clear() {
+    n1 = null;
+    n2 = null;
+    operate = null;
+ }
+
+
+ function getValue(event) {
+    let val = event.target.className;
+    console.log(val);
+    if (val == "num")
+        document.getElementById("display").innerHTML = event.target.id;
+ }
+
+ let num1, num2;
+ let operator;
+
+ let calcDiv = document.querySelectorAll(".calc-numbers > div");
+
+ for (let i = 0; i < calcDiv.length; i++) {
+    // console.log(calcDiv[i].attributes.alt.nodeValue);
+
+    calcDiv[i].addEventListener('click', getValue);
+    // calcDiv[i].addEventListener('click', event => {
+        
+    //     let val = event.target.title;
+    //     console.log(val);
+    //     if (!val)
+    //         console.log("Value does not exist");
+    //     // console.log(val);
+        
+    // });
+ }
+
+//  console.log(calcDiv);
